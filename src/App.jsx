0add79d4b293e19e95009bc458db83117aaead9f
Cybersport <Cybersport@26kadr.internal>
2025-11-28 11:42:56 +0300

    isdhfgjshgkj

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import BodyComponent from './components/Body';
+import UslugiComponent from './components/Uslugi'
+import './index.css'
+
+function App(){
+  return(
+    <div>
+      <BodyComponent />
+      <UslugiComponent />
+    </div>
+  )
+}
+
+export default App
